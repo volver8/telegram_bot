3a51d6cd0ebeed8fdc4a18722420ef4ebdb1b90c
Rusakov Vladimir <rvs1998@yandex.ru>
2024-06-21 03:02:13 +0300

    6 commit

--- homework.py
+++ homework.py
@@ -1,15 +1,14 @@
-# Очень крутая тема с isort, спасибо!
 import datetime
 import logging
 import os
 import sys
 import time
-import telebot  # type: ignore
 from http import HTTPStatus
 
-import requests  # type: ignore
+import requests
+import telebot
 from dotenv import load_dotenv
-from telebot import TeleBot  # type: ignore
+from telebot import TeleBot
 
 from exceptions import HomeworkStatusException, StatusCodeException
 
@@ -36,8 +35,8 @@ HOMEWORK_VERDICTS = {
 }
 
 # Формат записи для хендлера
-# Добавил func_name, а для чего добавлять str для не понял
-_log_format = '%(asctime)s, %(levelname)s, %(funcName)s, %(message)s, %(name)s'
+# Добавил lineno
+_log_format = '%(asctime)s, %(levelname)s, %(funcName)s,%(lineno)d, %(message)s, %(name)s'
 
 
 def get_file_handler():
@@ -71,16 +70,19 @@ logger = get_logger(__name__)
 
 def check_tokens():
     """Проверка токенов."""
+    missing_tokens = []
     for token in SOURCE:
         if not globals()[token]:
-            logger.critical(f'Отсутвует токен: {token}!')
-            sys.exit()
+            missing_tokens.append(token)
+    if len(missing_tokens) != 0:
+        logger.critical(f'Отсутвует токены: {missing_tokens}!')
+        sys.exit(1)
 
 
 def send_message(bot, message):
     """Отправка сообщения."""
+    logger.debug('Начало отправки сообщения...')
     try:
-        logger.debug('Начало отправки сообщения...')
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
         logger.debug('Сообщение отправлено')
     except telebot.apihelper.ApiException:
@@ -89,25 +91,20 @@ def send_message(bot, message):
 
 def get_api_answer(timestamp):
     """Запрос к API."""
+    logger.debug('Начало отправки запроса к API...')
     try:
-        logger.debug('Начало отправки запроса к API...')
         response = requests.get(
             ENDPOINT,
             headers=HEADERS,
             params={'from_date': timestamp}
         )
     except requests.exceptions.RequestException as error:
-        logger.critical(f'Ошибка отправки запроса! Ошибка: {error}')
         raise ConnectionError(f'Ошибка подключения! Ошибка: {error}')
     else:
         if response.status_code != HTTPStatus.OK:
-            logger.critical(
-                'Status code отличен от 200!'
-                'Status_code: ', response.status_code
-            )
             raise StatusCodeException(
                 'Status code отличен от 200!'
-                'Status_code: ', response.status_code
+                'Status_code: ', response.reason
             )
         logger.debug('Запрос к API успешно отправлен.')
         return response.json()
@@ -117,24 +114,15 @@ def check_response(response):
     """Проверка типа данных в ответе API."""
     logger.debug('Начало проверки ответа API...')
     if not isinstance(response, dict):
-        logger.error(
+        raise TypeError(
             'В ответе API структура данных не соответствует ожиданиям'
             'Ожидается dict: ', type(response)
         )
-        raise TypeError(
-            'В ответе API структура данных не соответствует ожиданиям'
-        )
-    elif 'homeworks'not in response:
-        logger.error('В ответе API отсутствует ключ homeworks')
+    elif 'homeworks' not in response:
         raise KeyError('В ответе API отсутствует ключ homeworks')
-    elif 'current_date'not in response:
-        logger.error('В ответе API отсутствует ключ current_date')
+    elif 'current_date' not in response:
         raise KeyError('В ответе API отсутствует ключ current_date')
     elif not isinstance(response.get('homeworks'), list):
-        logger.error(
-            'В ответе API домашки под ключом homeworks'
-            'данные приходят не в виде списка.'
-        )
         raise TypeError(
             'В ответе API домашки под ключом homeworks'
             'данные приходят не в виде списка!'
@@ -148,7 +136,6 @@ def parse_status(homework):
     logger.debug('Начало проверки статуса ответа API...')
 
     if homework.get('homework_name') is None:
-        logger.error('Отсутствует ключ homework_name!')
         raise KeyError(
             'Отсутствует ключ homework_name!'
         )
@@ -156,7 +143,6 @@ def parse_status(homework):
     homework_name = homework.get('homework_name')
 
     if homework.get('status') is None:
-        logger.error('Отсутствует ключ status!')
         raise KeyError(
             'Отсутствует ключ status!'
         )
@@ -185,32 +171,30 @@ def main():
 
     # Начальные переменные для проверки
     last_message = None
-    last_error_message = None
 
     while True:
         try:
             response = get_api_answer(timestamp)
             check_response(response)
+            homeworks = response.get('homeworks')
 
-            if response.get('homeworks') is None:
+            if homeworks is None:
                 logger.debug('Список с домашками пуст.')
 
             else:
                 homework = response.get('homeworks')[0]
                 message = parse_status(homework)
-                # Я так и не понял, нужно ли сравнивать что-то.
-                if message != last_message:
-                    send_message(bot, message)
+                send_message(bot, message)
                 last_message = message
 
             timestamp = response['current_date']
 
         except Exception as error:
             error_message = f'Сбой в работе программы: {error}'
-            if last_error_message != error_message:
+            logger.error(error_message)
+            if error_message != last_message:
                 send_message(bot, error_message)
-                logger.error(error_message)
-                last_error_message = error_message
+                last_message = error_message
 
         finally:
             time.sleep(RETRY_PERIOD)
